00b6c431a58c7e97b4166957085d4a51459f863d
Shashi Liyanage <liyanagemadushani21@gmail.com>
2023-10-09 12:53:37 -0400

    Initial Commit

---
+++ RelationshipReview/RelationshipReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelationshipReviewApp.swift
+//  RelationshipReview
+//
+//  Created by Shashi Liyanage on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RelationshipReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
