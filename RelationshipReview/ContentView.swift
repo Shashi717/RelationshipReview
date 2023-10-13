e92f2197ceabc877004b6042877a8818adfdfcf3
Shashi Liyanage <liyanagemadushani21@gmail.com>
2023-10-13 11:37:22 -0400

    Move code to PromptsView

--- RelationshipReview/ContentView.swift
+++ RelationshipReview/ContentView.swift
@@ -16,37 +16,7 @@ struct ContentView: View {
   var body: some View {
     VStack {
       if let prompts = promptsViewModel.prompts, !prompts.isEmpty {
-        PromptView(promptViewModel: prompts[currentIndex]).padding(EdgeInsets(top: 32, leading: 32, bottom: 32, trailing: 32))
-        HStack {
-          // make sure index doesn't go out of bounds
-          Button(action: {
-            if currentIndex > 0 {
-              currentIndex -= 1
-            }
-          }, label: {
-            Text("Previous")
-          }).buttonStyle(.bordered)
-          
-          Button(action: {
-            if currentIndex < prompts.count - 1 {
-              currentIndex += 1
-            }
-          }, label: {
-            Text("Next")
-          }).buttonStyle(.bordered)
-          
-          Button(action: {
-            if promptsViewModel.isCompleted {
-              presentAlert = false
-              promptsViewModel.submitAnswers()
-            } else {
-              presentAlert = true
-            }
-          }, label: {
-            Text("Submit")
-          }).buttonStyle(.bordered)
-        }
-        .alert("Please complete all the prompts", isPresented: $presentAlert, actions: {})
+        PromptsView(promptsViewModel: promptsViewModel)
       } else {
         // Temp for debugging
         Button(action: {
---
+++ RelationshipReview/PromptsView.swift
@@ -0,0 +1,53 @@
+//
+//  PromptsView.swift
+//  RelationshipReview
+//
+//  Created by Shashi Liyanage on 10/13/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct PromptsView: View {
+  @ObservedObject var promptsViewModel: PrompstViewModel
+  @State var currentIndex = 0
+  @State var presentAlert = false
+
+  var body: some View {
+    VStack {
+      if let prompts = promptsViewModel.prompts, !prompts.isEmpty {
+        PromptView(promptViewModel: prompts[currentIndex]).padding(EdgeInsets(top: 32, leading: 32, bottom: 32, trailing: 32))
+        HStack {
+          // make sure index doesn't go out of bounds
+          Button(action: {
+            if currentIndex > 0 {
+              currentIndex -= 1
+            }
+          }, label: {
+            Text("Previous")
+          }).buttonStyle(.bordered)
+
+          Button(action: {
+            if currentIndex < prompts.count - 1 {
+              currentIndex += 1
+            }
+          }, label: {
+            Text("Next")
+          }).buttonStyle(.bordered)
+
+          Button(action: {
+            if promptsViewModel.isCompleted {
+              presentAlert = false
+              promptsViewModel.submitAnswers()
+            } else {
+              presentAlert = true
+            }
+          }, label: {
+            Text("Submit")
+          }).buttonStyle(.bordered)
+        }
+        .alert("Please complete all the prompts", isPresented: $presentAlert, actions: {})
+      }
+    }
+  }
+}
