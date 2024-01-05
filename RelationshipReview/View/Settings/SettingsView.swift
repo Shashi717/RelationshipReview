03ab9f534a61f60cd2b5e19e8294df0314b92536
Shashi Liyanage <liyanagemadushani21@gmail.com>
2024-01-04 21:49:48 -0500

    changes

--- RelationshipReview/View/Settings/SettingsView.swift
+++ RelationshipReview/View/Settings/SettingsView.swift
@@ -10,14 +10,16 @@ import SwiftUI
 struct SettingsView: View {
     @Binding var isLoggedIn: Bool
     var body: some View {
-        VStack {
-            EditUserInfoView()
-                .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
-        }
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("Sign out") {
-                    signOut()
+        NavigationStack {
+            VStack {
+                EditUserInfoView()
+                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Sign out") {
+                        signOut()
+                    }
                 }
             }
         }
